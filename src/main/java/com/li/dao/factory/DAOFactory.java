62b0fa035105eae9d493f859d0bd748a53709088
fjl121029xx <sishengqikuo_xx@163.com>
2018-05-15 00:02:09 +0800

    用户行为分析

---
+++ src/main/java/com/li/dao/factory/DAOFactory.java
@@ -0,0 +1,21 @@
+package com.li.dao.factory;
+
+import com.li.dao.ITaskDAO;
+import com.li.dao.impl.TaskDAOImpl;
+
+/**
+ * DAO工厂类
+ * @author Administrator
+ *
+ */
+public class DAOFactory {
+
+	/**
+	 * 获取任务管理DAO
+	 * @return DAO
+	 */
+	public static ITaskDAO getTaskDAO() {
+		return new TaskDAOImpl();
+	}
+	
+}
---
+++ src/test/java/com/li/analysisuserbehaviors/TaskDAOTest.java
@@ -0,0 +1,21 @@
+package com.li.analysisuserbehaviors;
+
+
+import com.li.dao.ITaskDAO;
+import com.li.dao.factory.DAOFactory;
+import com.li.domain.Task;
+
+/**
+ * 任务管理DAO测试类
+ * @author Administrator
+ *
+ */
+public class TaskDAOTest {
+	
+	public static void main(String[] args) {
+		ITaskDAO taskDAO = DAOFactory.getTaskDAO();
+		Task task = taskDAO.findById(2);
+		System.out.println(task.getTaskName());  
+	}
+	
+}
